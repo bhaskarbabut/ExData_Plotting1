f15b1cc0b0a3d4dd99cf466d2dd104de8d1db50c
bhaskarbabut <66745369+bhaskarbabut@users.noreply.github.com>
2021-01-28 17:33:47 +0530

    plot1 - histogram

--- Plot1.R
+++ Plot1.R
@@ -1,1 +1,46 @@
+# Read the contents of the file household_power_consumption.txt
+df <- read.table("household_power_consumption.txt",
+                header = TRUE,
+                sep = ";",
+                na.strings = "?",
+                colClasses = c("character", "character",
+                                "numeric", "numeric",
+                                "numeric", "numeric",
+                                "numeric", "numeric", "numeric")
+                               )
+
+
+## Format date to Type Date
+df$Date <- as.Date(df$Date, "%d/%m/%Y")
+
+
+## Filter data set from Feb. 1, 2007 to Feb. 2, 2007
+df <- subset(df, Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+## Remove incomplete observation
+df <- df[complete.cases(t),]
+
+## Combine Date and Time column
+dateTime <- paste(df$Date, df$Time)
+
+## Name the vector
+dateTime <- setNames(dateTime, "DateTime")
+
+## Remove Date and Time column
+df <- df[, !(names(t) %in% c("Date", "Time"))]
+
+## Add DateTime column
+df <- cbind(dateTime, df)
+
+## Format dateTime Column
+df$dateTime <- as.POSIXct(dateTime)
+
+
+
+## Create the histogram
+hist(df$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+
+## Save to plot1.png
+dev.copy(png, "plot1.png", width = 480, height = 480)
+dev.off()
 
